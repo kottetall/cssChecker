53344843d38ce3c939b4c1290be18ba9c6e13a5f
kottetall <taikes_bass@hotmail.com>
2019-09-24 15:42:45 +0200

    huvudfunktionen fixad

--- app.js
+++ app.js
@@ -3,23 +3,30 @@
 const fs = require("fs");
 const fileCheck = "./css/testfil.css" //filen som ska läsas
 const fileSource = fs.readdirSync("./properties/") // hämtar alla grundfiler från moz - array med filnamn + ändelse
-
-
-
-
-// console.log(fileSource)
-fs.readFile("./properties/" + fileSource[100], "utf-8", (err, string) => {
-    console.log(string)
-})
-
+const browsers = [
+    "chrome",
+    "chrome_android",
+    "edge",
+    "firefox",
+    "firefox_android",
+    "ie",
+    "opera",
+    "safari",
+    "safari_ios",
+    "webview_android"
+]
 
 fs.readFile(fileCheck, "utf-8", (err, string) => {
     // läser in testfil test.css
     if (err) {
         console.log(err)
     } else {
-        const propVals = letaProps(string)
-        // console.log(propVals)
+        const propVals = letaProps(string) //Kollar vilka "egenskaper" som finns
+        const props = findCommon(Object.keys(propVals), fileSource) //Hittar de filer från Moz som är kopplade till resp egenskap
+        const customJson = createCustomJson(props["commonProps"]) //Skapar Json med de egenskaper som finns i projektet tillsammans med vilka webbläsare/versioner som de är kompatibla med
+        const compatList = compatCheck(customJson) //Listar vad som är komp med resp webbläsare och sorterar detta utifrån den första versionen när det blev tillagd
+        // console.log(compatList)
+        console.log(props["missingProps"])
     }
 })
 
@@ -41,4 +48,68 @@ function letaProps(string) {
         }
     }
     return propVals
+}
+
+function findCommon(check, source) {
+    const commonProps = []
+    const missingProps = []
+
+    for (let i = 0; i < source.length; i++) {
+        source[i] = source[i].replace(".json", "")
+    }
+
+    for (const c of check) {
+        if (source.includes(c)) {
+            commonProps.push(c)
+        } else {
+            missingProps.push(c)
+        }
+    }
+
+    return {
+        commonProps: commonProps,
+        missingProps: missingProps
+    }
+}
+
+function createCustomJson(commonProps) {
+    const customJson = {}
+    for (const prop of commonProps) {
+
+        let temp = require(`./properties/${prop}.json`)
+
+        customJson[prop] = {}
+        for (const browser of browsers) {
+            customJson[prop][browser] = temp.css.properties[prop]["__compat"].support[browser]
+        }
+    }
+    return customJson
+}
+
+function compatCheck(customJson) {
+    const overview = {}
+
+    for (const browser of browsers) {
+        overview[browser] = []
+    }
+
+    for (const custom in customJson) {
+        for (const browser of browsers) {
+            if (!Array.isArray(customJson[custom][browser])) {
+                overview[browser].push([custom, customJson[custom][browser].version_added])
+            } else {
+                overview[browser].push([custom, customJson[custom][browser][0].version_added])
+            }
+        }
+    }
+
+
+    for (const over in overview) {
+        overview[over].sort((a, b) => {
+            a[1] = a[1].toString().replace("≤", "")
+            b[1] = b[1].toString().replace("≤", "")
+            return (a[1] === false) ? -1 : b[1] - a[1]
+        })
+    }
+    return overview
 }
